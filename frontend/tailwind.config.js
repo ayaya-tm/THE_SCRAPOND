d1bf6acfd2916fe97ea3db0e7cbb4a8c3b204ca0
ayaya-tm <backnumber1128@icloud.com>
2025-06-19 22:35:24 +0900

    UI改善

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,14 @@
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      animation: {
+        'pulse-slow': 'pulse 5s ease-in-out infinite',
+      },
+    },
+  },
+  plugins: [],
+}
