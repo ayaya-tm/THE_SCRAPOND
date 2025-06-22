e4d32af68bb05ef96ee0ea6521e1f0bcc296e0de
ayaya-tm <backnumber1128@icloud.com>
2025-06-22 12:10:13 +0900

    docs

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,9 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/THE_SCRAPOND/',
-  build: {
-    outDir: 'docs',         // ← これでルートに docs/ ができる
-  },
+
   plugins: [react()],
 })
